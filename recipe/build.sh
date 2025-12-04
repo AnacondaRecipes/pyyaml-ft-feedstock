e4a1865e08441a841d29b16d9ba05a96449d6178
Andrii Osipov <aosipov@anaconda.com>
2025-12-04 05:31:53 -0800

    PKG-11218: pyyaml-ft 8.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,4 @@
 export PYYAML_FORCE_CYTHON=1
 export PYYAML_FORCE_LIBYAML=1
 
-${PYTHON} -m pip install .                  \
-    -vv                                     \
-    --no-deps                               \
-    --no-build-isolation
+${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
